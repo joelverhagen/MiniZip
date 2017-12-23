19c1a860edfe645d47457cdfdc4fa36a43e23a47
Joel Verhagen <joel.verhagen@gmail.com>
2017-12-22 21:49:47 -0800

    Fix things up in AssemblyInfo.cs

--- MiniZip/Properties/AssemblyInfo.cs
+++ MiniZip/Properties/AssemblyInfo.cs
@@ -2,23 +2,24 @@
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion(
-    ThisAssembly.Git.SemVer.Major + "." +
-    ThisAssembly.Git.SemVer.Minor + "." +
-    ThisAssembly.Git.SemVer.Patch)]
+    ThisAssembly.Git.BaseVersion.Major + "." +
+    ThisAssembly.Git.BaseVersion.Minor + "." +
+    ThisAssembly.Git.BaseVersion.Patch)]
 
 [assembly: AssemblyFileVersion(
-    ThisAssembly.Git.SemVer.Major + "." +
-    ThisAssembly.Git.SemVer.Minor + "." +
-    ThisAssembly.Git.SemVer.Patch + ".0")]
+    ThisAssembly.Git.BaseVersion.Major + "." +
+    ThisAssembly.Git.BaseVersion.Minor + "." +
+    ThisAssembly.Git.BaseVersion.Patch + ".0")]
 
 [assembly: AssemblyInformationalVersion(
-  ThisAssembly.Git.SemVer.Major + "." +
-  ThisAssembly.Git.SemVer.Minor + "." +
-  ThisAssembly.Git.SemVer.Patch +
+  ThisAssembly.Git.BaseVersion.Major + "." +
+  ThisAssembly.Git.BaseVersion.Minor + "." +
+  ThisAssembly.Git.BaseVersion.Patch +
   ThisAssembly.Git.SemVer.DashLabel)]
 
 [assembly: AssemblyMetadata("CommitHash", ThisAssembly.Git.Sha)]
 [assembly: AssemblyMetadata("Branch", ThisAssembly.Git.Branch)]
 [assembly: AssemblyMetadata("IsDirty", ThisAssembly.Git.IsDirtyString)]
+[assembly: AssemblyMetadata("Tag", ThisAssembly.Git.Tag)]
 
 [assembly:InternalsVisibleTo("Knapcode.MiniZip.Test")]
