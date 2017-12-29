e09c52803f99da4d2ed9ff6eabbe5272832ac2c6
Joel Verhagen <joel.verhagen@gmail.com>
2017-12-28 21:24:51 -0800

    Revert "Add some dope assembly info"
    This reverts commit a818094c187a16cc9959d94ace99132bc3391c6c.

--- MiniZip/Properties/AssemblyInfo.cs
+++ MiniZip/Properties/AssemblyInfo.cs
@@ -1,24 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyVersion(
-    ThisAssembly.Git.SemVer.Major + "." +
-    ThisAssembly.Git.SemVer.Minor + "." +
-    ThisAssembly.Git.SemVer.Patch)]
-
-[assembly: AssemblyFileVersion(
-    ThisAssembly.Git.SemVer.Major + "." +
-    ThisAssembly.Git.SemVer.Minor + "." +
-    ThisAssembly.Git.SemVer.Patch + ".0")]
-
-[assembly: AssemblyInformationalVersion(
-  ThisAssembly.Git.SemVer.Major + "." +
-  ThisAssembly.Git.SemVer.Minor + "." +
-  ThisAssembly.Git.SemVer.Patch +
-  ThisAssembly.Git.SemVer.DashLabel)]
-
-[assembly: AssemblyMetadata("CommitHash", ThisAssembly.Git.Sha)]
-[assembly: AssemblyMetadata("Branch", ThisAssembly.Git.Branch)]
-[assembly: AssemblyMetadata("IsDirty", ThisAssembly.Git.IsDirtyString)]
+﻿using System.Runtime.CompilerServices;
 
 [assembly:InternalsVisibleTo("Knapcode.MiniZip.Test")]
